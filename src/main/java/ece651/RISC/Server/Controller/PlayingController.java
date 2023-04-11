4f83b93eee8a3d20947993957a61c59aef490715
Yang Ouyang <yo46@duke.edu>
2023-04-12 02:00:58 +0800

    Using lock and Condition to solve the sychronization problem, finish the playing controller, without test.

--- src/main/java/ece651/RISC/Server/Controller/PlayingController.java
+++ src/main/java/ece651/RISC/Server/Controller/PlayingController.java
@@ -1,58 +1,67 @@
 package ece651.RISC.Server.Controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import ece651.RISC.Server.Model.Game;
+import ece651.RISC.Server.Model.OnlineServer2Client;
+import ece651.RISC.Server.Model.Round;
+import ece651.RISC.shared.AttackAction;
+import ece651.RISC.shared.MoveAction;
 import ece651.RISC.shared.Player;
 import ece651.RISC.shared.Status;
+import ece651.RISC.shared.Territory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.CountDownLatch;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 @Slf4j
 @RestController
 public class PlayingController {
     @Autowired
     public Game serverGame;
-
-    private final CountDownLatch latch = new CountDownLatch(3);
-
+    @Autowired
+    public OnlineServer2Client msgMaker;
+    private final Lock lock = new ReentrantLock();
+    private final Condition actionComplete = lock.newCondition();
     @GetMapping("/playing")
-    public String greeting(
-            @RequestParam(value = "data", defaultValue = "Nick") String reqJSON) throws InterruptedException {
+    public String greeting(@RequestBody String actionsJSON) throws InterruptedException {
         // check status first
         if (serverGame.getStatus() != Status.gameStatus.PLAYING) {
             // return HTTP.serverError;
             return "error";
         }
 
+        System.out.println("receiveActions" + actionsJSON);
+        JSONObject jsonObject = JSON.parseObject(actionsJSON);
+        String playerJSON = jsonObject.getString("player");
+        Player player = JSON.parseObject(playerJSON, Player.class);
+        String moveActionsJSON = jsonObject.getString("moveActions");
+        String attackActionsJSON = jsonObject.getString("attackActions");
+        List<MoveAction> moveActions = JSON.parseArray(moveActionsJSON, MoveAction.class);
+        List<AttackAction> attackActions = JSON.parseArray(attackActionsJSON, AttackAction.class);
+        serverGame.recieveAction(player, (ArrayList<MoveAction>) moveActions, (ArrayList<AttackAction>) attackActions);
 
-
-        // TODO: handle reqJSON
-        // {"id":1,"name":"test","num_units":10,"owner":{"id":1,"name":"cp","status":3},"unit":10}
-        int playerId = 1;
-        Set<Integer> territoriesId = new HashSet<>();
-        territoriesId.add(1);
-        int unit = 10;
-
-        serverGame.OperateFromPlayer(); //playerId, territoriesId, unit
-
-        if (serverGame.getOperatedPlayerSize() < 3) {
-            latch.await();
-        } else {
-
-            latch.countDown();
+        lock.lock();
+        try {
+            if (serverGame.getAllocatedPlayerSize() < 3) {
+                actionComplete.await();
+            } else {
+                actionComplete.signalAll();
+            }
+        } finally {
+            lock.unlock();
         }
 
-        System.out.println("after latch *******");
 
-        // server2Client
-        // TODO: return updated player info and map info
-        Player updatedPlayer = serverGame.getPlayer(playerId);
-        return updatedPlayer.toJSON();
+        return msgMaker.oneTurnMsg(serverGame.getMyMap(), serverGame.getPlayer(player.getId()).getStatus(), serverGame.getStatus());
     }
 }
