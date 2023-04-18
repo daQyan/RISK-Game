1903d5fbf7656084b8a45bd2d0081e4a1a0f2ba3
Yang Ouyang <yo46@duke.edu>
2023-04-19 01:37:01 +0800

    trying to debug with the attack and move part by replacing JSON Objects with id, but not make sense

--- src/main/java/ece651/RISC/shared/Model/ActionsRequest.java
+++ src/main/java/ece651/RISC/shared/Model/ActionsRequest.java
@@ -3,31 +3,31 @@ package ece651.RISC.shared.Model;
 import java.util.List;
 
 public class ActionsRequest {
-    private List<MoveAction> moveActions;
-    private List<AttackAction> attackActions;
-    private List<UpgradeAction> upgradeActions;
+    private List<RequestMoveAction> moveActions;
+    private List<RequestAttackAction> attackActions;
+    private List<ReqeustUpgradeAction> upgradeActions;
 
-    public List<MoveAction> getMoveActions() {
+    public List<RequestMoveAction> getMoveActions() {
         return moveActions;
     }
 
-    public void setMoveActions(List<MoveAction> moveActions) {
+    public void setMoveActions(List<RequestMoveAction> moveActions) {
         this.moveActions = moveActions;
     }
 
-    public List<AttackAction> getAttackActions() {
+    public List<RequestAttackAction> getAttackActions() {
         return attackActions;
     }
 
-    public void setAttackActions(List<AttackAction> attackActions) {
+    public void setAttackActions(List<RequestAttackAction> attackActions) {
         this.attackActions = attackActions;
     }
 
-    public List<UpgradeAction> getUpgradeActions() {
+    public List<ReqeustUpgradeAction> getUpgradeActions() {
         return upgradeActions;
     }
 
-    public void setUpgradeActions(List<UpgradeAction> upgradeActions) {
+    public void setUpgradeActions(List<ReqeustUpgradeAction> upgradeActions) {
         this.upgradeActions = upgradeActions;
     }
 }
