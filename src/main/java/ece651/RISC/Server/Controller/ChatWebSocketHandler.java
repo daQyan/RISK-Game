d856ae3881bfa781a73eae40aad46c1dd55910ec
Zilin Xu <zx112@duke.edu>
2023-05-04 21:01:50 +0800

    chatroom

--- src/main/java/ece651/RISC/Server/Controller/ChatWebSocketHandler.java
+++ src/main/java/ece651/RISC/Server/Controller/ChatWebSocketHandler.java
@@ -27,12 +27,11 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 /**
  * 这里，我们维护了一个 Map<String, WebSocketSession> 对象，它将每个 player 的 id 映射到其对应的 WebSocketSession。
  * 在 afterConnectionEstablished 回调中，我们从 WebSocket 连接的 URI 中解析出 player id 并将其加入到 playerSessions 映射中。
- * <p>
  * 在 handleTextMessage 回调中，我们检查接收到的消息是否包含空格，如果不包含，就将消息广播给所有连接的用户；
  * 如果包含空格，则我们将第一个空格之前的部分解析为接收方的 player id，并使用
  */
 @Component
-@WebSocketEndpoint("/chat")
+@WebSocketEndpoint("/{gameID}/chat")
 public class ChatWebSocketHandler extends TextWebSocketHandler {
     // Maintain a list of all currently connected WebSocket sessions
     private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
@@ -46,9 +45,15 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessions.add(session);
+//        String playerId = session.getUri().getQuery().split("=")[1];
+        String uri = session.getUri().getPath();
+        String[] uriParts = uri.split("/");
+        String gameId = uriParts[1];
         String playerId = session.getUri().getQuery().split("=")[1];
         playerSessions.put(playerId, session);
-        broadcast("Player " + playerId + " joined the chat");
+        String broadcastMessage = "Game " + gameId + ", Player " + playerId + " join the chat";
+        broadcast(broadcastMessage, gameId);
+
     }
 
     /**
@@ -59,9 +64,13 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         String playerId = session.getUri().getQuery().split("=")[1];
-        String broadcastMessage = "Player " + playerId + ": " + payload;
-        broadcast(broadcastMessage);
+        String gameId = session.getUri().getPath().split("/")[1];
+        String broadcastMessage = "Game " + gameId + ", Player " + playerId + ": " + payload;
+        broadcast(broadcastMessage, gameId);
+
+
     }
+
 
     /**
      * Callback for when a WebSocket connection is closed.
@@ -70,14 +79,28 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         sessions.remove(session);
+//        String playerId = session.getUri().getQuery().split("=")[1];
+        String uri = session.getUri().getPath();
+        String[] uriParts = uri.split("/");
+        String gameId = uriParts[1];
         String playerId = session.getUri().getQuery().split("=")[1];
+
         playerSessions.remove(playerId);
-        broadcast("Player " + playerId + " left the chat");
+        String broadcastMessage = "Game " + gameId + ", Player " + playerId + " left the chat";
+        broadcast(broadcastMessage, gameId);
+
     }
 
-    private void broadcast(String message) throws IOException {
+    private void broadcast(String message, String gameId) throws IOException {
         for (WebSocketSession session : sessions) {
-            session.sendMessage(new TextMessage(message));
+            String uri = session.getUri().getPath();
+            String[] uriParts = uri.split("/");
+            String sessionGameId = uriParts[1];
+            if (sessionGameId.equals(gameId)) {
+                session.sendMessage(new TextMessage(message));
+            }
         }
     }
+
+
 }
