9ee280ee1e3d4a7bf2ae6eadddd91bc03ceb4271
Zilin Xu <zx112@duke.edu>
2023-05-04 22:13:44 +0800

    chatroom finish: diff game room and parse with json

--- src/main/java/ece651/RISC/Server/Controller/ChatWebSocketHandler.java
+++ src/main/java/ece651/RISC/Server/Controller/ChatWebSocketHandler.java
@@ -1,106 +1,72 @@
 package ece651.RISC.Server.Controller;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ece651.RISC.Server.config.WebSocketEndpoint;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-/**
- * WebSocket handler for the chat room.
- * Handles incoming WebSocket messages and broadcasts them to all connected users.
- * Also maintains a map of WebSocketSessions for each player to enable private messaging.
- */
-
-/**
- * 这里，我们维护了一个 Map<String, WebSocketSession> 对象，它将每个 player 的 id 映射到其对应的 WebSocketSession。
- * 在 afterConnectionEstablished 回调中，我们从 WebSocket 连接的 URI 中解析出 player id 并将其加入到 playerSessions 映射中。
- * 在 handleTextMessage 回调中，我们检查接收到的消息是否包含空格，如果不包含，就将消息广播给所有连接的用户；
- * 如果包含空格，则我们将第一个空格之前的部分解析为接收方的 player id，并使用
- */
+import java.util.HashMap;
+import java.util.Map;
 @Component
 @WebSocketEndpoint("/{gameID}/chat")
 public class ChatWebSocketHandler extends TextWebSocketHandler {
-    // Maintain a list of all currently connected WebSocket sessions
-    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
-    // Maintain a map of WebSocket sessions for each player
     private final Map<String, WebSocketSession> playerSessions = new HashMap<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    /**
-     * Callback for when a new WebSocket connection is established.
-     * Adds the new session to the list of connected sessions.
-     */
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessions.add(session);
-//        String playerId = session.getUri().getQuery().split("=")[1];
-        String uri = session.getUri().getPath();
-        String[] uriParts = uri.split("/");
-        String gameId = uriParts[1];
-        String playerId = session.getUri().getQuery().split("=")[1];
+        String playerId = getPlayerIdFromSessionUri(session.getUri().getQuery());
+        String gameId = getGameIdFromSessionUri(session.getUri().getPath());
         playerSessions.put(playerId, session);
-        String broadcastMessage = "Game " + gameId + ", Player " + playerId + " join the chat";
+        String broadcastMessage = String.format("Game %s, Player %s join the chat", gameId, playerId);
         broadcast(broadcastMessage, gameId);
-
     }
 
-    /**
-     * Callback for when a WebSocket message is received.
-     * Broadcasts the message to all connected users or sends a private message to a specific player.
-     */
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
-        String playerId = session.getUri().getQuery().split("=")[1];
-        String gameId = session.getUri().getPath().split("/")[1];
-        String broadcastMessage = "Game " + gameId + ", Player " + playerId + ": " + payload;
+        String playerId = getPlayerIdFromSessionUri(session.getUri().getQuery());
+        String gameId = getGameIdFromSessionUri(session.getUri().getPath());
+
+        // Parse the message as JSON
+        Map<String, Object> messageJson = objectMapper.readValue(payload, HashMap.class);
+        int id = (int) messageJson.get("playerID");
+        String text = messageJson.get("text message").toString();
+
+        // Broadcast the message as a JSON object
+        Map<String, Object> broadcastJson = new HashMap<>();
+        broadcastJson.put("gameID", gameId);
+        broadcastJson.put("playerID", id);
+        broadcastJson.put("text message", text);
+        String broadcastMessage = objectMapper.writeValueAsString(broadcastJson);
         broadcast(broadcastMessage, gameId);
-
-
     }
 
-
-    /**
-     * Callback for when a WebSocket connection is closed.
-     * Removes the session from the list of connected sessions.
-     */
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        sessions.remove(session);
-//        String playerId = session.getUri().getQuery().split("=")[1];
-        String uri = session.getUri().getPath();
-        String[] uriParts = uri.split("/");
-        String gameId = uriParts[1];
-        String playerId = session.getUri().getQuery().split("=")[1];
-
+        String playerId = getPlayerIdFromSessionUri(session.getUri().getQuery());
+        String gameId = getGameIdFromSessionUri(session.getUri().getPath());
         playerSessions.remove(playerId);
-        String broadcastMessage = "Game " + gameId + ", Player " + playerId + " left the chat";
+        String broadcastMessage = String.format("Game %s, Player %s left the chat", gameId, playerId);
         broadcast(broadcastMessage, gameId);
-
     }
 
-    private void broadcast(String message, String gameId) throws IOException {
-        for (WebSocketSession session : sessions) {
-            String uri = session.getUri().getPath();
-            String[] uriParts = uri.split("/");
-            String sessionGameId = uriParts[1];
+    private String getPlayerIdFromSessionUri(String uriQuery) {
+        return uriQuery.split("=")[1];
+    }
+
+    private String getGameIdFromSessionUri(String uriPath) {
+        return uriPath.split("/")[1];
+    }
+
+    private void broadcast(String message, String gameId) throws Exception {
+        for (WebSocketSession session : playerSessions.values()) {
+            String sessionGameId = getGameIdFromSessionUri(session.getUri().getPath());
             if (sessionGameId.equals(gameId)) {
                 session.sendMessage(new TextMessage(message));
             }
         }
     }
-
-
 }
